7b0f86e10ba4d8ae119e69caed0d0b8e0d536a4a
Maxim <sein.maks@bk.ru>
2021-10-31 18:54:19 +0200

    edit files

--- main.cpp
+++ main.cpp
@@ -1,36 +1,10 @@
-/*Вариант 3.
-Создайте 3 прямоугольника разных цветов и размеров. 
-Перемещайте их из исходного положения влево до 
-конца экрана с разной скорость. При достижении конца 
-экрана остановите фигуры.
-*/
 #include <SFML/Graphics.hpp>
 
-
-int main(){
-
-
-    sf::RenderWindow window(sf::VideoMode(1920, 1280), L"Летающие прямоугольники! Вау!");
-
-    int x_1 = window.getSize().x, x_2 = window.getSize().x, x_3 = window.getSize().x;
-
-    //прямоугольник первый
-    sf::RectangleShape rectangle1(sf::Vector2f(300.f, 150.f));
-    rectangle1.setFillColor(sf::Color::Yellow);
-    rectangle1.setOrigin(rectangle1.getSize().x/2,rectangle1.getSize().y/2);
-    rectangle1.setPosition(x_1,300);
-
-    //прямоугольник второй
-    sf::RectangleShape rectangle2(sf::Vector2f(400.f, 200.f));
-    rectangle2.setFillColor(sf::Color::Red);
-    rectangle2.setOrigin(rectangle2.getSize().x/2,rectangle2.getSize().y/2);
-    rectangle2.setPosition(x_2,600);
-
-    //прямоугольник третий
-    sf::RectangleShape rectangle3(sf::Vector2f(500.f, 250.f));
-    rectangle3.setFillColor(sf::Color::Green);
-    rectangle3.setOrigin(rectangle3.getSize().x/2,rectangle3.getSize().y/2);
-    rectangle3.setPosition(x_3,900);
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
 
     while (window.isOpen())
     {
@@ -41,26 +15,9 @@ int main(){
                 window.close();
         }
 
-        if(x_1 > 0) // Если нам нужно движение слева направо, то вместо >0 можно написать < window.getSize().x
-                    // Сначала я сделал так, поэтому решил поделиться
-            x_1-=1;
-        rectangle1.setPosition(x_1,300);
-
-        if(x_2 > 0)
-            x_2-=2;
-        rectangle2.setPosition(x_2,600);
-
-        if(x_3 > 0)
-            x_3-=3;
-        rectangle3.setPosition(x_3,900);
-
         window.clear();
-        window.draw(rectangle1);
-        window.draw(rectangle2);
-        window.draw(rectangle3);
+        window.draw(shape);
         window.display();
-
-        window.setFramerateLimit(60);
     }
 
     return 0;
